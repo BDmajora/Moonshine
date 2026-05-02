ef8970369633b995b6c3aa2c436b534112da1e71
BDMajora <bdmajora@protonmail.com>
2026-05-01 21:02:00 -0600

    game is basically done with the cards being placed correctly now

--- programs/solitaire/sol_draw.c
+++ programs/solitaire/sol_draw.c
@@ -1,7 +1,11 @@
 #include "solitaire.h"
 #include "sol_draw.h"
 
-/* Drag state (Defined here, externed in solitaire.h for sol_input.c to use) */
+/* Standard cards.dll modes for the special empty deck markers */
+#define MODE_DECKX 6  /* Red X - no cards left to deal */
+#define MODE_DECKO 7  /* Green Circle - click to reshuffle waste into stock */
+
+/* Drag state */
 BOOL  dragging = FALSE;
 CARD  drag_cards[52];
 int   drag_count = 0;
@@ -27,11 +31,9 @@ void DrawBoard(HDC hdc, int width, int height) {
     DWORD elapsed;
 
     /* 1. Status Bar */
-    rcStatus.left = 0;
-    rcStatus.top = height - STATUS_BAR_HEIGHT;
-    rcStatus.right = width;
-    rcStatus.bottom = height;
-
+    rcStatus.left = 0; rcStatus.top = height - STATUS_BAR_HEIGHT;
+    rcStatus.right = width; rcStatus.bottom = height;
+    
     hbr = CreateSolidBrush(RGB(255, 255, 255));
     FillRect(hdc, &rcStatus, hbr);
     DeleteObject(hbr);
@@ -43,37 +45,47 @@ void DrawBoard(HDC hdc, int width, int height) {
     SelectObject(hdc, hOldPen);
     DeleteObject(hpen);
 
-    hfont = CreateFontW(16, 0, 0, 0, FW_BOLD, 0, 0, 0, ANSI_CHARSET,
-        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
-        DEFAULT_PITCH | FF_SWISS, L"MS Sans Serif");
+    hfont = CreateFontW(16, 0, 0, 0, FW_BOLD, 0, 0, 0, ANSI_CHARSET, 
+                        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, 
+                        DEFAULT_PITCH | FF_SWISS, L"MS Sans Serif");
     hOldFont = (HFONT)SelectObject(hdc, hfont);
     SetBkMode(hdc, TRANSPARENT);
 
     elapsed = (GetTickCount() - g_Game.start_tick) / 1000;
     wsprintfW(buf, L"Score: %d  Time: %d", g_Game.score, (int)elapsed);
-    rcText = rcStatus;
-    rcText.right -= STATUS_MARGIN;
+    rcText = rcStatus; rcText.right -= STATUS_MARGIN;
     DrawTextW(hdc, buf, -1, &rcText, DT_SINGLELINE | DT_VCENTER | DT_RIGHT);
 
     SelectObject(hdc, hOldFont);
     DeleteObject(hfont);
 
-    /* 2. Stock - Restored to top-left fanning (Up and Left) */
+    /* 2. Stock - Original 3D Effect (Thickness on Top/Left, Pulling from Bottom/Right) */
     if (g_Game.stock_top > 0) {
-        if (g_Game.stock_top > 5)
-            cdtDraw(hdc, X_MARGIN - 4, Y_MARGIN - 4, CARD_BACK_RED, MODE_FACEDOWN, SOL_BG_COLOR);
-        if (g_Game.stock_top > 1)
-            cdtDraw(hdc, X_MARGIN - 2, Y_MARGIN - 2, CARD_BACK_RED, MODE_FACEDOWN, SOL_BG_COLOR);
+        /* 
+         * Logic: The "Anchor" is the back card. The front card is offset +4, +4.
+         * As the deck empties, the front layers are removed first, making it 
+         * look like you are pulling from the top and the deck is shrinking back.
+         */
+        
+        // Layer 1: The back-most base (Always drawn)
         cdtDraw(hdc, X_MARGIN, Y_MARGIN, CARD_BACK_RED, MODE_FACEDOWN, SOL_BG_COLOR);
+        
+        // Layer 2: Middle thickness (Drawn if deck has more than 5 cards)
+        if (g_Game.stock_top > 5)
+            cdtDraw(hdc, X_MARGIN + 2, Y_MARGIN + 2, CARD_BACK_RED, MODE_FACEDOWN, SOL_BG_COLOR);
+            
+        // Layer 3: The top-most card (Drawn if deck has more than 10 cards)
+        if (g_Game.stock_top > 10)
+            cdtDraw(hdc, X_MARGIN + 4, Y_MARGIN + 4, CARD_BACK_RED, MODE_FACEDOWN, SOL_BG_COLOR);
     } else {
-        /* Show the re-deal circle (card 53) if there are cards in the waste */
+        /* Draw the Reshuffle (O) or Empty (X) markers */
         if (g_Game.waste_top > 0)
-            cdtDraw(hdc, X_MARGIN, Y_MARGIN, 53, MODE_FACEUP, SOL_BG_COLOR);
+            cdtDraw(hdc, X_MARGIN, Y_MARGIN, 0, MODE_DECKO, SOL_BG_COLOR); 
         else
-            cdtDraw(hdc, X_MARGIN, Y_MARGIN, 0, MODE_GHOST, SOL_BG_COLOR);
+            cdtDraw(hdc, X_MARGIN, Y_MARGIN, 0, MODE_DECKX, SOL_BG_COLOR); 
     }
 
-    /* 3. Waste - Subtle Diagonal fanning (1-pixel vertical step) */
+    /* 3. Waste - Subtle Diagonal fanning */
     if (g_Game.waste_top > 0) {
         int show = g_Game.waste_top < 3 ? g_Game.waste_top : 3;
         int wx_base = X_MARGIN + X_SPACING;
@@ -85,8 +97,8 @@ void DrawBoard(HDC hdc, int width, int height) {
             if (dragging && drag_from_type == SRC_WASTE && i == show - 1)
                 continue;
 
-            cdtDraw(hdc, wx, wy,
-                    g_Game.waste[g_Game.waste_top - show + i] & ~CARD_FACEUP,
+            cdtDraw(hdc, wx, wy, 
+                    g_Game.waste[g_Game.waste_top - show + i] & ~CARD_FACEUP, 
                     MODE_FACEUP, SOL_BG_COLOR);
         }
     }
@@ -97,8 +109,8 @@ void DrawBoard(HDC hdc, int width, int height) {
         if (g_Game.found_top[i] == 0)
             cdtDraw(hdc, fx, Y_MARGIN, 0, MODE_GHOST, SOL_BG_COLOR);
         else
-            cdtDraw(hdc, fx, Y_MARGIN,
-                    g_Game.foundation[i][g_Game.found_top[i] - 1] & ~CARD_FACEUP,
+            cdtDraw(hdc, fx, Y_MARGIN, 
+                    g_Game.foundation[i][g_Game.found_top[i] - 1] & ~CARD_FACEUP, 
                     MODE_FACEUP, SOL_BG_COLOR);
     }
 
@@ -133,7 +145,7 @@ void DrawBoard(HDC hdc, int width, int height) {
         int dx = drag_mouse_x - drag_x_off;
         int dy = drag_mouse_y - drag_y_off;
         for (i = 0; i < drag_count; i++)
-            cdtDraw(hdc, dx, dy + (i * FACE_UP_OFF),
+            cdtDraw(hdc, dx, dy + (i * FACE_UP_OFF), 
                     drag_cards[i] & ~CARD_FACEUP, MODE_FACEUP, SOL_BG_COLOR);
     }
 }
